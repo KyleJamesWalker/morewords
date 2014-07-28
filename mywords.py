b2d130ca961ab88c26f260c6215c125f013e217f
Kyle Walker <Kyle.Walker@zefr.com>
2014-07-27 21:56:24 -0700

    Redis Cache, with light rest support.

--- mywords.py
+++ mywords.py
@@ -2,23 +2,21 @@
 from __future__ import unicode_literals
 
 import os
+import json
 import time
 import tornado.httpserver
-import tornado.ioloop
 import tornado.options
 import tornado.web
+import redis
 
 from collections import defaultdict
+from functools import wraps
+from tornado.ioloop import IOLoop
 from tornado.options import define, options
 
 from trie import Trie
 
 define("port", default=8888, help="run on the given port", type=int)
-
-t0 = time.time()
-words_collection = Trie("OWL2.txt")
-t1 = time.time()
-print "Trie Loaded: {}ms".format(round((t1-t0) * 1000, 2))
 
 
 class Application(tornado.web.Application):
@@ -37,8 +35,81 @@ class Application(tornado.web.Application):
         )
         tornado.web.Application.__init__(self, handlers, **settings)
 
+        # Global Connection to Words
+        t0 = time.time()
+        self.words_collection = Trie("OWL2.txt")
+        t1 = time.time()
+        print "Trie Loaded: {}ms".format(round((t1-t0) * 1000, 2))
 
-class MainHandler(tornado.web.RequestHandler):
+        self.r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        self.r.flushdb()
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def redis(self):
+        return self.application.r
+
+    @property
+    def words_collection(self):
+        return self.application.words_collection
+
+    def get(self, *args, **kwargs):
+        self.content_type = 'application/json'
+        self.write(self.get_resource(*args, **kwargs))
+        self.finish()
+
+    def get_resource(self, resource=None):
+        raise NotImplementedError()
+
+    def post_resource(self, resource=None):
+        raise NotImplementedError()
+
+    def patch_resource(self, resource=None):
+        raise NotImplementedError()
+
+    def delete_resource(self, resource=None):
+        raise NotImplementedError()
+
+
+def cached(timeout=60 * 60, future=True):
+    '''
+        Simple Cache decorator for a BaseHandler. Requires cache value to be
+            setup externally to function.
+
+        :param timeout: Number of seconds for cache to live. Default 1hour.
+        :param key: String to format key.  Note: Format passed key with flask
+            request.full_path
+    '''
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            self = args[0]
+            uri = self.request.uri
+
+            rv = self.redis.get(uri)
+
+            while rv == "FUTURE":
+                # Relax and try again
+                time.sleep(0.01)
+                rv = self.redis.get(uri)
+
+            if rv is not None:
+                return json.loads(rv)
+
+            # Notify we're building a responce (expire in 3 seconds)
+            self.redis.setex(uri, 3, "FUTURE")
+
+            rv = f(*args, **kwargs)
+
+            self.redis.setex(uri, 3600, json.dumps(rv))
+            return rv
+
+        return decorated_function
+    return decorator
+
+
+class MainHandler(BaseHandler):
     def get(self):
         # TODO: Front-end goes here.
         doc_str = ("Find what words you can spell<br>"
@@ -46,47 +117,25 @@ class MainHandler(tornado.web.RequestHandler):
         self.write(doc_str)
 
 
-class WordHandler(tornado.web.RequestHandler):
-    # TODO: A cache engine should handle this.
-    # Save last 10 responses
-    requests = list()
-    responces = list()
-
-    def get(self, word):
-        # Bad Caching Example
-        # Note this could be a future (aka all other threads)
-        # will hold for the first to finish?
-        uri = self.request.uri
-        if uri in self.requests:
-            self.write(self.responces[self.requests.index(uri)])
-            return
-
-        # Actual Code
+class WordHandler(BaseHandler):
+    @cached()
+    def get_resource(self, word):
         word = word.upper()
         distance = int(self.get_argument('distance', '0'))
 
         words = defaultdict(list)
-        for x in words_collection.get_words(word, distance):
+        for x in self.words_collection.get_words(word, distance):
             words[len(x)].append(x)
 
-        resp = {'word': word,
+        return {'word': word,
                 'words': words}
-
-        # Bad Caching Example
-        self.requests.append(uri)
-        self.responces.append(resp)
-        if len(self.requests) > 10:
-            self.requests.pop(0)
-            self.responces.pop(0)
-
-        self.write(resp)
 
 
 def main():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+    IOLoop.instance().start()
 
 
 if __name__ == "__main__":
